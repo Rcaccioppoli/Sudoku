1f77c858bd124287d369b79a70f24b5b4c956681
Rcaccioppoli <Caccioppolirocco@gmail.com>
2016-11-25 18:41:21 +0100

    Da correggere "createGrids" e terminare i test.

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -1,9 +1,139 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
-public class SudokuVerifier {
+import java.util.ArrayList;
 
+public class SudokuVerifier {
+	
 	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
-		return 0;
+		int result;
+		
+		if (!isNumeric(candidateSolution))
+			result=-1;
+		else if (!onceInAGrid(candidateSolution))
+			result=-2;
+		else if (!onceInARow(candidateSolution))
+			result=-3;
+		else if (!onceInAColumn(candidateSolution))
+			result=-4;
+		else
+			result=0;
+		
+		return result;
+	}
+
+	private boolean onceInAColumn(String candidateSolution) {
+		boolean valid=true;
+		ArrayList<String> columns = crateColumns(candidateSolution);
+		
+		for (int i=0; (i<columns.size() && valid); i++)
+		{
+			if(!oneDigits(columns.get(i)))
+				valid=false;
+		}
+		
+		return valid;
+	}
+
+	private ArrayList<String> crateColumns(String candidateSolution) {
+		ArrayList<String> columns = new ArrayList<>();
+		ArrayList<String> rows = createRows(candidateSolution);
+		String toAdd = "";
+		
+		for (int i=0; i<rows.size(); i++){
+			toAdd="";
+			for (int j=0; j<rows.size(); j++) {
+				toAdd+=rows.get(j).charAt(i);
+			}
+			columns.add(toAdd);	
+		}
+
+		return columns;
+	}
+
+	private boolean onceInARow(String candidateSolution) {
+		boolean valid=true;
+		ArrayList<String> rows = createRows(candidateSolution);
+		
+		for (int i=0; (i<rows.size() && valid); i++)
+		{
+			if(!oneDigits(rows.get(i)))
+				valid=false;
+		}
+		
+		return valid;
+	}
+
+	private ArrayList<String> createRows(String candidateSolution) {
+		ArrayList<String> rows = new ArrayList<>();
+		String toAdd = "";
+		
+		for (int j=0; j<candidateSolution.length(); j=j+9){
+			toAdd = candidateSolution.substring(j, j+9);
+			rows.add(toAdd);
+			toAdd = "";
+		}
+
+		return rows;
+	}
+
+	private boolean onceInAGrid(String candidateSolution) {
+		boolean valid=true;
+		ArrayList<String> grids = createGrids(candidateSolution);
+		
+		for (int i=0; (i<grids.size() && valid); i++)
+		{
+			if(!oneDigits(grids.get(i)))
+				valid=false;
+		}
+		
+		return valid;
+	}
+
+	private boolean oneDigits(String string) {
+		boolean result=true;
+		
+		for (int i=0; i<string.length(); i++) {
+			for (int j=i+1; j<string.length(); j++) {
+				if(string.charAt(i)==string.charAt(j))
+					result=false;
+			}
+		}
+		
+		return result;
+	}
+
+	private ArrayList<String> createGrids(String candidateSolution) {
+		ArrayList<String> grids = new ArrayList<>();
+		String toAdd = "";
+		
+		for (int j=0; j<candidateSolution.length()+6; j=j+6){
+			
+			if(j==81)
+				j=j/27;
+			else if (j==84)
+				j=j/14;
+			
+			toAdd += candidateSolution.charAt(j++);
+			toAdd += candidateSolution.charAt(j++);
+			toAdd += candidateSolution.charAt(j++);
+			
+			if (toAdd.length()==9){
+				grids.add(toAdd);
+				toAdd = "";
+			}
+		}
+			
+		return grids;
+	}
+
+	private boolean isNumeric(String candidateSolution) {
+		boolean valid=true;
+
+		for(int i=0; (i<candidateSolution.length() && valid); i++) {
+			if (!Character.isDigit(candidateSolution.charAt(i)))
+				valid=false;
+		}
+		
+		return valid;
 	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,13 +2,36 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	String correctString;
+	String incorrectString;
+	SudokuVerifier sudoku;
+	String notNumericString;
+	
+	@Before
+	public void setup() {
+		correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		incorrectString = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		notNumericString = "C234567899123456788912345I778912345667891234556789123445A78912334567891223456789O";
+		sudoku= new SudokuVerifier();
 	}
-
+	
+	@Test
+	public void isNumericTest() {
+		assertEquals(0, sudoku.verify(correctString));
+		assertEquals(-2, sudoku.verify(incorrectString));
+		assertEquals(-1, sudoku.verify(notNumericString));
+	}
+	
+	@Test
+	public void onceInAGridTest()
+	{
+		assertEquals(0, sudoku.verify(correctString));
+		assertEquals(-2, sudoku.verify(incorrectString));
+		assertEquals(-1, sudoku.verify(notNumericString));
+	}
 }
